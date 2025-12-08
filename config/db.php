14ce1e1d17b19a40cd76e8c2e85ada98ef3fd738
EJjara <ejaraca@unsa.edu.pe>
2025-12-08 15:17:44 -0500

    ga

--- config/db.php
+++ config/db.php
@@ -1,16 +1,17 @@
 <?php
 class Database {
-    private $host = "localhost";
+    private $host = "crossover.proxy.rlwy.net";
+    private $port = 25173;
     private $dbname = "inventario_ventas";
     private $username = "root";
-    private $password = "";
+    private $password = "EEhfIhkFWNzNnsGLHfGfyrkxRIomnqDh";
     private $conn;
 
     public function getConnection() {
         $this->conn = null;
 
         try {
-            $dsn = "mysql:host={$this->host};dbname={$this->dbname};charset=utf8mb4";
+            $dsn = "mysql:host={$this->host};port={$this->port};dbname={$this->dbname};charset=utf8mb4";
             $this->conn = new PDO($dsn, $this->username, $this->password);
             $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             $this->conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
